e8a0d65e3966a5795af1a344ccde6d500988b0e2
jannamohamed <jannaradwan2022@gmail.com>
2022-10-29 20:12:43 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function BMI ( weight , height ) {
+   let result = weight / (height * height)  
+    return result
+}
+console.log ( BMI( 50, 1.6)) 
 
+function Status (BodyMass) {
+     if ( BodyMass > 18.5) {
+       return " لديك نقص في الوزن "
+     } else if (BodyMass <= 18.5 && BodyMass > 25 ) {
+        return " وزنك صحي "
+     } else ( BodyMass >= 25 ) 
+        return " لديك زيادة في الوزن " 
+     } 
+
+function calculate ( ) {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi_value = BMI ( weight , height )
+    let desc = Status (bmi_value)
+    let box = document.getElementById('result')
+    box.innerText = bmi_value + " == " + desc
+}
